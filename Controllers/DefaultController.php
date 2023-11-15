79705d74e7ea81c2b0785cd5563db05a9b6df1c9
Téo Salmeron <teo.salmeron2601@gmail.com>
2023-11-15 13:38:02 +0100

    Made navbar

--- Controllers/DefaultController.php
+++ Controllers/DefaultController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controllers;
 
+use App\Models\GarageModel;
+
 class DefaultController extends Controller
 {
     public function index()
     {
-        $this->render("home", [
-            "title" => "Accueil"
-        ]);
+        $garage_model = new GarageModel;
+        $garage = $garage_model->findBy(["id" => 1])[0];
+        $this->render("home/home", [
+            "title" => "Accueil",
+            "garage" => $garage
+        ], ["nav"]);
 
     }
 }
---
+++ Views/templates/nav.php
@@ -0,0 +1,22 @@
+<nav id="nav">
+    <a href="/">
+        <img src="/img/logo.png" alt="">
+    </a>
+    <button id="toggleMenu">
+        <img src="/img/toggle_menu.png" alt="">
+    </button>
+    <ul id="navList">
+        <button id="closeMenu">
+            <img src="/img/close_menu.png" alt="">
+        </button>
+        <li>
+            <a href="/">Accueil</a>
+        </li>
+        <li>
+            <a href="/voitures">Véhicules d'occasion</a>
+        </li>
+        <li>
+            <a href="/connexion">Espace personnel</a>
+        </li>
+    </ul>
+</nav>
